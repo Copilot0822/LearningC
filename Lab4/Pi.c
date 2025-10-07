e00ce1c2f09c822f2ce6b47ff5c67e297b4bc3e1
Copilot0822 <123857131+Copilot0822@users.noreply.github.com>
2025-10-07 19:32:48 -0400

    Add Pi approximation program using series
    Introduced Pi.c, which calculates an approximation of Pi using a series expansion based on user input for the number of terms. Added corresponding compiled files Pi.exe and Pi.obj.

---
+++ Lab4/Pi.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+bool isOdd(int number){
+    return (number % 2) != 0;
+}
+int main()
+{
+    
+    long double pi = 4;
+
+    long double terms = 1;
+    long double num = 3;
+
+    printf("Enter number of terms: ");
+
+    int numterms;
+
+    scanf("%d", &numterms);
+
+    for(int i = 1;i<=numterms;i++){
+        if(isOdd(i)){
+            terms -=(1/num);
+        }
+        else{
+            terms +=(1/num);
+        }
+
+        //terms += 1/num;
+        // printf("%Lf\n", terms);
+        num +=2;
+    }
+    pi = pi*terms;
+
+    printf("pi is: %Lf", pi);
+
+
+    return 0;
+}
